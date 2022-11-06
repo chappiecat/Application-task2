5a56a71810d35429a4624a986356115d06bef2bc
EC2 Default User <ec2-user@ip-172-31-20-157.ap-northeast-1.compute.internal>
2022-11-06 15:29:36 +0000

    Initial Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -14,11 +14,19 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+
   def update
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully."
     else
-      render "show"
+      render "edit"
     end
   end
 
@@ -28,10 +36,21 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
+  def is_matching_login_user
+      user_id = params[:id].to_i
+      login_user_id = current_user.id
+      if(user_id != login_user_id)
+                redirect_to books_path
+      end
+  end
+
+
   def ensure_correct_user
     @user = User.find(params[:id])
     unless @user == current_user
       redirect_to user_path(current_user)
     end
+  end
+  end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  devise_for :users
+
   get "home/about" => "homes#about", as: "about"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
